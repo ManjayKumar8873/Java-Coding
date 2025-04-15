cc6cfcbccda8ec8fdcf1b004dc6bb19e33520f0f
Manjay Kumar <mkp88736@gmail.com>
2025-04-15 02:51:45 +0000

    reverse

---
+++ java coding/reverse.java
@@ -0,0 +1,22 @@
+
+public class reverse {
+    public static void main(String[] args) {
+        String s = "manjay";
+
+        //  using for loop
+
+        int len = s.length();
+        String rev = "";
+
+        for(int i = len-1; i>=0; i--){
+            rev = rev + s.charAt(i); 
+        }
+        System.out.println(rev);
+
+        // using StringBuffer
+
+        StringBuffer sf = new StringBuffer(s);
+        System.out.println(sf.reverse());
+
+    }
+}
